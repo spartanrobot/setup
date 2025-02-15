25d99a4b64ee558d1daa0bdf1a367bb73c8de878
CyanoJ <86079319+CyanoJ@users.noreply.github.com>
2025-02-15 18:34:03 -0500

    Make GPIO work again

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_i2c 1
 sudo raspi-config nonint do_spi 0
 sudo raspi-config nonint do_serial_hw 0
 sudo raspi-config nonint do_ssh 0
 sudo raspi-config nonint do_camera 0
 sudo raspi-config nonint disable_raspi_config_at_boot 0
+# Enable 1Wire
 
 sudo apt update
 sudo apt-get install -y python3-pip git i2c-tools libgpiod-dev python3-libgpiod python-scipy
@@ -23,6 +25,10 @@ sudo cp /home/admin/bt_ip/bt_ip.service /lib/systemd/system/bt_ip.service
 sudo chmod 644 /lib/systemd/system/bt_ip.service
 sudo systemctl daemon-reload
 sudo systemctl enable bt_ip.service
+
+# Fix GPIO issues
+sudo pip3 uninstall --break-system-packages adafruit-blinka
+sudo pip3 install --break-system-packages git+https://github.com/frank-pet/Adafruit_Blinka.git@651192bdb5c1d97e5d5e401e0dd17a0d65c15371
 
 read -p "Do you want to reboot now? [Y/n] " -n 1 -r
 echo    
